59dabba888b98630406f18a4f6ca4f35b3e445e4
DanielFariias <danielfariias15@gmail.com>
2024-02-20 11:26:21 -0300

    refactor: :fire: Add error icon to Input component

--- src/view/components/input.tsx
+++ src/view/components/input.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps, forwardRef } from 'react'
 import { cn } from '../../app/utils/cn'
+import { XCircle } from '@phosphor-icons/react'
 
 interface InputProps extends ComponentProps<'input'> {
   name: string
@@ -19,21 +20,24 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
           name={name}
           className={cn(
             'bg-white w-full rounded-lg border placeholder-shown:pt-0 pt-4 border-gray-400 px-3 h-[48px] text-gray-800 focus:border-gray-800 outline-none transition-all peer',
-            error && 'border-red-900 focus:border-red-900',
+            error && 'border-red-500 focus:border-red-500',
             className,
           )}
           placeholder=" "
         />
         <label
           htmlFor={name}
-          className="absolute text-xs top-1.5 left-[13px] pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-[11px] transition-all"
+          className={cn(
+            'absolute text-xs top-1.5 left-[13px] pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-[11px] transition-all',
+            error && 'text-red-500',
+          )}
         >
           {placeholder}
         </label>
 
         {error && (
-          <div className="flex gap-2 items-center mt-2 text-red-900">
-            {/* <CrossCircledIcon /> */}
+          <div className="flex gap-1 items-center mt-2 text-red-500">
+            <XCircle />
             <span className="text-xs">{error}</span>
           </div>
         )}
