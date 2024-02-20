24c13e5b2bc659c8c222cf3a0e7aaf795ddb953a
DanielFariias <danielfariias15@gmail.com>
2024-02-20 11:27:17 -0300

    Add AuthProvider to App component

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -1,9 +1,10 @@
+import { AuthProvider } from '@/app/contexts/auth-context'
 import { Router } from '../app/router'
 
 export function App() {
   return (
-    <>
+    <AuthProvider>
       <Router />
-    </>
+    </AuthProvider>
   )
 }
