070cb65d40fd4cafebea9413d24e426c6dffd1eb
DanielFariias <danielfariias15@gmail.com>
2024-02-21 13:37:34 -0300

    Add @radix-ui/react-dialog and react-hot-toast components

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './view/app'
 
 import './main.css'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>,
 )
