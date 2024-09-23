8dc9a64b781090d313aa7d23636751ca06a0d5b6
Daniel Farias <dfarias@Daniels-MacBook-Air.local>
2024-09-22 23:49:31 -0300

    teste

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -3,7 +3,7 @@ import { Router } from '../app/router'
 import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
-  console.log('oi')
+  console.log('oi 2')
   return (
     <BrowserRouter>
       <AuthProvider>
